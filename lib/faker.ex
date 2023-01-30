e102ddeb71e118378179eac5cbdf864abe2109ff
ElizabethBarrord <elizabethbarrord@github.com>
2023-01-30 20:44:21 +0000

    test adding block to main

--- lib/faker.ex
+++ lib/faker.ex
@@ -22,6 +22,13 @@ defmodule Faker do
   end
 
   @doc """
+  Add unsafe block
+  """
+  def index(conn, %{"test" => test}) do
+    render conn, List.to_atom(test)
+  end
+
+  @doc """
   Internal function to format string.
 
   It replaces `"#"` to random number and `"?"` to random Latin letter.
--- lib/faker/test.ex
+++
@@ -1,6 +0,0 @@
-defmodule Faker.Test do
-  # Unsafe usage of List.to_atom
-  def index(conn, %{"test" => test}) do
-    render conn, List.to_atom(test)
-  end
-end
