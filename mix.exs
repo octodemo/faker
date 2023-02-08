080d22f379a19437917f8c598e8e66f6fd6084dd
ElizabethBarrord <elizabethbarrord@github.com>
2023-02-08 00:42:20 +0000

    adding jason

--- mix.exs
+++ mix.exs
@@ -43,7 +43,7 @@ defmodule Faker.Mixfile do
     [
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
       {:earmark, ">= 0.0.0", only: :dev, runtime: false},
-      {:jason, "~> 1.3", only: :dev, runtime: false},
+      {:jason, "~> 1.3"},
       {:dialyxir, "~> 1.0", only: [:dev], runtime: false}
     ]
   end
